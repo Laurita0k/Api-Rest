8c7b91ab7fd1730e8c263153186c3ec9d421e6f7
Laurita <laurita.dl.2790@gmail.com>
2023-07-31 04:38:47 -0600

    asd

--- SRC/app.js
+++ SRC/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
-const routes = require('./routes');
+const app = express('express');
+const routes = require('./Routes');
+const src = require('./SRC')
 
 const products = require('/routes/ProductRoutes')
 app.use('/api/ProductRoutes' ,products);
 
 app.listen(3000, () => console.log(`server corriendo en el puerto http://localhost:3000/`));
-
