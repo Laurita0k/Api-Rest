662090bdef11bbc3695b24ee46584a21b950d4a3
Laurita <laurita.dl.2790@gmail.com>
2023-07-31 03:56:05 -0600

    asdasd

--- SRC/Routes/ProductRoutes.js
+++ SRC/Routes/ProductRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-
+const products = require('products')
 
 router.get('/ProductRoutes.js', productController.getAllProducts);
 
